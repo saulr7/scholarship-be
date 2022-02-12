c56207df6616704cdcdf7d7629a58681d189cd2b
Saul Ramirez <fernando.ramire@hotmail.com>
2022-02-12 11:36:24 -0600

    :art: adding scholaship routes

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,8 @@
 import { Application } from "express"
 import authRouter from "./api/controllers/auth/router"
+import scholarshipRouter from "./api/controllers/schoolarship/routes"
 
 export default function routes(app: Application): void {
 	app.use("/auth", authRouter)
+	app.use("/scholarship", scholarshipRouter)
 }
