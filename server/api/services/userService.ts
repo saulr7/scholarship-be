227a7846d9c1b9e814fda56c63860feef098976e
Saul Ramirez <fernando.ramire@hotmail.com>
2022-02-12 11:35:54 -0600

    :recycle: moving jwt logic

--- server/api/services/userService.ts
+++ server/api/services/userService.ts
@@ -1,14 +1,28 @@
 import UserModel from "../../../shared/models/UserModel"
 import knex from "../../common/database"
 import IService from "./IService"
+import { PrismaClient } from '@prisma/client'
 
 export default class UserService implements IService<UserModel> {
+
+	private prismaClient: PrismaClient
+
+	constructor() {
+		this.prismaClient = new PrismaClient()
+	}
+
 	validate(model: UserModel): Promise<boolean> {
 		throw new Error("Method not implemented.")
 	}
-	create(model: UserModel): Promise<UserModel> {
-		throw new Error("Method not implemented.")
+	async create(model: UserModel): Promise<UserModel> {
+		try {
+			return await this.prismaClient.user.create({ data: { ...model } })
+		} catch (error) {
+			return Promise.reject(error)
+		}
 	}
+
+
 	update(model: UserModel): Promise<UserModel> {
 		throw new Error("Method not implemented.")
 	}
@@ -32,13 +46,7 @@ export default class UserService implements IService<UserModel> {
 
 	public async getByEmail(emailAddress: string): Promise<UserModel> {
 		try {
-			var query = knex<UserModel>("user")
-				.where("emailAddress", emailAddress)
-				.first()
-				.select()
-			const user: UserModel = await query
-
-			return user
+			return await this.prismaClient.user.findFirst({ where: { emailAddress } })
 		} catch (error) {
 			return Promise.reject(error)
 		}
